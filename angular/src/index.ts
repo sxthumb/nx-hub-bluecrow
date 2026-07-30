3e6370baa8d2986dac2948c1ee55a3b570185976
DESKTOP-KM47ILD\Usuário <tjusto2409@gmail.com>
2026-07-30 11:51:14 -0300

    chore:remove angular in root

--- angular/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './lib/atoms/atoms';
--- angular/src/lib/atoms/atoms.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-atoms',
-  imports: [],
-  templateUrl: './atoms.html',
-  styleUrl: './atoms.css'
-})
-export class Atoms {}
